bf0cf25e64c5e84685ac7becf3f92a0a69ee53fa
Abdulrahman Agboola-Osho <111765959+agboola-osho@users.noreply.github.com>
2026-01-24 11:19:21 +0000

    added sample april tag code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystem/AprilTagTest.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+//import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+@TeleOp
+public class AprilTagTest extends OpMode {
+    private Limelight3A limelight;
+    private IMU imu;
+    @Override
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight"); //change this to device name
+        limelight.pipelineSwitch(8); //april tag change this!
+        imu = hardwareMap.get(IMU.class, "imu");
+//        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
+    }
+
+    @Override
+    public void start(){
+        limelight.start();
+    }
+
+    @Override
+    public void loop(){
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw(AngleUnit.DEGREES));
+        LLResult llresult = limelight.getLatestResult();
+        if(llresult != null && llresult.isValid()){
+            Pose3D botPose = llresult.getBotpose_MT2();
+            double distance = getDistanceFromTag(llresult.getTa());
+            telemetry.addData("distance", distance);
+
+            telemetry.addData("Tx", llresult.getTx());
+            telemetry.addData("Ty", llresult.getTy());
+            telemetry.addData("Ta", llresult.getTa());
+            telemetry.addData("Botpose", botPose.toString());
+        }
+    }
+    public double getDistanceFromTag(double ta){
+        double scale = 30665.95;
+        return (scale/ta);
+    }
+}
